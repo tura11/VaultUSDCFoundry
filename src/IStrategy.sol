ccf5657968df42fe54b8f903a978cfa85e0ca153
Tymek <tymek_uramek@wp.pl>
2025-10-06 21:31:29 +0200

    Add compelte IStrategy intreface

---
+++ src/IStrategy.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {VaultUSDC} from "./VaultUSDC.sol";
+
+interface IStrategy {
+    function deposit(uint256 amount) external returns (uint256);
+    function withdraw(uint256 amount) external returns (uint256);
+    function harvest() external returns (uint256);
+    function balanceOf() external view returns (uint256);
+    function asset() external view returns (address);
+    function isActive() external view returns (bool);
+    function emergencyWithdraw() external;
+
+}
+
