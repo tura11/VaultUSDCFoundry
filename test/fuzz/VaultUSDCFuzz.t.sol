7890ca62aad90b3fbfd2294674e5d970f4c1ee61
TymekU <tura11@users.noreply.github.com>
2025-10-20 12:33:23 +0200

    add fuzz testing

---
+++ test/fuzz/VaultUSDCFuzz.t.sol
@@ -0,0 +1,73 @@
+pragma solidity 0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {VaultUSDC} from "../../src/VaultUSDC.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {MockStrategy} from "../mocks/MockStrategy.sol";
+
+
+contarct VaultUSDCFuzzTest is Test {
+    VaultUSDC vault;
+    ERC20Mock usdc;
+    MockStrategy strategy;
+
+    address owner;
+    address user;
+
+
+    uint256 public constant MAX_USDC_SUPPLY = 1_000_000_000e6;
+
+
+    function setUp() public {
+        owner = makeAddr("owner");
+        user = makeAddr("user");
+
+
+        usdc = new ERC20Mock();
+        
+        vm.prank(owner);
+        vault = new VaultUSDC(usdc);
+        vm.stopPrank();
+
+
+        vm.prank(owner);
+        strategy = new MockStrategy(address(usdc), address(vault));
+        vm.stopPrank();
+
+        vm.prank(owner);
+        vault.setStrategy(address(strategy));
+        vm.stopPrank();
+
+        usdc.mint(user, MAX_USDC_SUPPLY);
+
+    }
+
+
+    function testFuzz_Deposit(uint256 amount) {
+
+        vm.assume(amount > 0);
+        vm.assume(amount <= MAX_USDC_SUPPLY);
+        vm.assume(amount <= vault.maxDepositLimit());
+
+
+        vm.startPrank(user);
+        usdc.approve(address(vault), amount);
+
+        uint256 balanceBefore = usdc.balanceOf(user);
+
+        uint256 shares =  vault.deposit(amount, user);
+        vm.stopPrank();
+        
+
+        assertLt(usdc.balanceOf(user), balanceBefore, "User balance should decrease");
+        
+    
+        assertGt(shares, 0, "Should receive shares");
+        
+        
+        assertEq(vault.balanceOf(user), shares, "Share balance mismatch");
+        
+        
+        assertGt(vault.totalDeposited(), 0, "Total deposited should increase");
+    }
+}
