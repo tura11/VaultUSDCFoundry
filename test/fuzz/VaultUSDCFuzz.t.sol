755f1bfa06b493f314baea64420bde3f6aafb369
TymekU <tura11@users.noreply.github.com>
2025-10-20 13:59:34 +0200

    more fuzz tests

--- test/fuzz/VaultUSDCFuzz.t.sol
+++ test/fuzz/VaultUSDCFuzz.t.sol
@@ -3,10 +3,10 @@ pragma solidity 0.8.24;
 import {Test, console} from "forge-std/Test.sol";
 import {VaultUSDC} from "../../src/VaultUSDC.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
-import {MockStrategy} from "../mocks/MockStrategy.sol";
+import {MockStrategy} from "../mocks/AaveStrategyMock.sol";
 
 
-contarct VaultUSDCFuzzTest is Test {
+contract VaultUSDCFuzzTest is Test {
     VaultUSDC vault;
     ERC20Mock usdc;
     MockStrategy strategy;
@@ -15,7 +15,7 @@ contarct VaultUSDCFuzzTest is Test {
     address user;
 
 
-    uint256 public constant MAX_USDC_SUPPLY = 1_000_000_000e6;
+    uint256 public constant MAX_USDC_SUPPLY = 1_000_000e6;
 
 
     function setUp() public {
@@ -43,7 +43,7 @@ contarct VaultUSDCFuzzTest is Test {
     }
 
 
-    function testFuzz_Deposit(uint256 amount) {
+    function testFuzz_Deposit(uint256 amount) public {
 
         vm.assume(amount > 0);
         vm.assume(amount <= MAX_USDC_SUPPLY);
@@ -70,4 +70,26 @@ contarct VaultUSDCFuzzTest is Test {
         
         assertGt(vault.totalDeposited(), 0, "Total deposited should increase");
     }
+
+
+   function testFuzz_DepositWithBound(uint256 randomAmount) public {
+        
+        uint256 amount = bound(randomAmount, 1, MAX_USDC_SUPPLY);
+
+        vm.startPrank(user);
+        usdc.approve(address(vault), amount);
+
+        uint256 balanceBefore = usdc.balanceOf(user);
+
+        uint256 expectedFee = (amount * vault.managementFee()) / 10000;
+        uint256 expectedAfterFee = amount - expectedFee;
+
+        uint256 shares = vault.deposit(amount, user);
+        vm.stopPrank();
+
+        assertApproxEqAbs(shares, expectedAfterFee, 1, "Shares should match amount after fee");
+        assertEq(usdc.balanceOf(user), balanceBefore - amount, "User balance should decrease by full amount");
+    }
+
+
 }
